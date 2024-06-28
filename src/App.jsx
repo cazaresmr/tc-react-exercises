fc7cfb34552eb999e1b0f535b04129d6870567f6
Michael Cazares <michaelcazares@gmail.com>
2024-06-28 14:50:10 -0700

    Removed HomePage components from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 // src/App.jsx or src/App.js
 
 import "./App.css";
-// import HomePage from "./components/HomePage";
 import FilmsPage from "./components/FilmsPage";
 
 function App() {
   return (
     <div className='App'>
-      {/* <HomePage /> */}
       <FilmsPage />
     </div>
   );
